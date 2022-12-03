b7fef1c7ddf85b157ef79c34683b5660f40aae1a
Yaroslav Yadryshnikov <me@yay.qa>
2022-12-04 01:04:48 +0300

    d3

--- 2022/d3/main.go
+++ 2022/d3/main.go
@@ -5,7 +5,48 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
+
+func cpp(ch rune) int { // Calculate priority points
+	val := rune(ch)
+	if ch > 96 {
+		val = ch - 96
+	} else {
+		val = ch - 38
+	}
+	return int(val)
+}
+
+func ol(sl string) int { // One liner handler
+	sllen := len(sl)
+	sp1 := sl[:sllen/2]
+	sp2 := sl[sllen/2:]
+	for _, ch := range sp1 {
+		cp := strings.IndexRune(sp2, ch)
+		if cp != -1 {
+			return cpp(ch)
+		}
+	}
+	return 0
+}
+
+func tl(sl []string) int {
+	cru := make([]rune, 0, 10)
+	for _, ch := range sl[0] {
+		cp := strings.IndexRune(sl[1], ch)
+		if cp != -1 {
+			cru = append(cru, ch)
+		}
+	}
+	for _, ch := range cru {
+		cp := strings.IndexRune(sl[2], ch)
+		if cp != -1 {
+			return cpp(ch)
+		}
+	}
+	return 0
+}
 
 func main() {
 	file, err := os.Open("input")
@@ -14,12 +55,25 @@ func main() {
 	}
 	defer file.Close()
 
+	sumol := 0
+	sumtl := 0
+	tlg := make([]string, 0, 3) // Three liner group
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		sl := scanner.Text()
+		sumol += ol(sl)
+		tlg = append(tlg, sl)
+		if len(tlg)%3 == 0 {
+			sumtl += tl(tlg)
+			tlg = tlg[:0]
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Println("Sum oneliner:", sumol)
+	fmt.Println("Sum threeliner:", sumtl)
 }
